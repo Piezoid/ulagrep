d4dad4ce9cedcde87f2e56ac1ba5c6af14baa67d
Maël Kerbiriou <piezo.w.dimd@gmail.com>
2020-09-20 14:50:57 +0200

    update benchmarks

--- ula/benches/randsse_benchmarks.rs
+++ ula/benches/randsse_benchmarks.rs
@@ -2,42 +2,12 @@ use criterion::*;
 
 use rand::distributions::{Distribution, Uniform};
 use rand::rngs::SmallRng;
-use rand::{Rng, SeedableRng};
+use rand::SeedableRng;
 
 use triple_accel::levenshtein::{levenshtein_search_simd_with_opts, LEVENSHTEIN_COSTS};
 use triple_accel::SearchType;
 
 pub use ula::*;
-
-pub fn make_random_string(seed: u64, size: usize, card: u8) -> Box<[u8]> {
-    assert!(card > 0);
-    Uniform::from(97u8..97u8.checked_add(card).unwrap())
-        .sample_iter(SmallRng::seed_from_u64(seed))
-        .take(size)
-        .collect()
-}
-
-fn bench_random(c: &mut Criterion) {
-    const N: usize = 4 << 20;
-    let txt = black_box(make_random_string(0, N, 4));
-    let pat = black_box(make_random_string(1, 20, 4));
-
-    for k in [7].iter().cloned() {
-        let mut g = c.benchmark_group(format!("random_string_k{}", k));
-        g.throughput(Throughput::Bytes(N as u64));
-
-        let mut vec = Vec::new();
-        g.bench_function("ulasse", |b| {
-            b.iter(|| {
-                vec.clear();
-                ula::search(k, pat.as_ref(), txt.as_ref(), &mut vec);
-                vec.len();
-            })
-        });
-
-        g.finish();
-    }
-}
 
 use std::io::Result;
 use std::path::Path;
@@ -84,9 +54,11 @@ fn bench_genome(c: &mut Criterion) {
     bench_data(
         c,
         "https://github.com/smart-tool/smart/raw/master/data/genome/ecoli.txt",
-        1 << 20,
+        1 << 19,
     )
 }
+
+use std::time::Duration;
 
 fn bench_data(c: &mut Criterion, url: &str, size: usize) {
     let (data, name) = get_url_data(url, "../data").expect("Could not get benchmark data");
@@ -95,21 +67,27 @@ fn bench_data(c: &mut Criterion, url: &str, size: usize) {
 
     let mut group = c.benchmark_group(name);
     group.throughput(Throughput::Bytes(size as u64));
-
+    group.plot_config(PlotConfiguration::default().summary_scale(AxisScale::Logarithmic));
+    group.warm_up_time(Duration::from_secs(1));
+    group.measurement_time(Duration::from_secs(30));
+    group.sample_size(50);
+    
     let mut results = Vec::new();
 
     for k in 1..=7 {
-        for &len in [2, 4, 8, 16, 32, 64, 128, 256, 512].iter() {
-            if len < k+1 {
+        for &len in [1, 2, 4, 6, 8, 10, 16, 32, 64, 128, 256, 512].iter() {
+            if len < k + 1 {
                 continue;
             }
+
+            let mut pats = Uniform::from(0..(size - len))
+                .sample_iter(SmallRng::seed_from_u64(0))
+                .map(|i| &data[i..i + len]);
+
             group.bench_with_input(
                 BenchmarkId::new("ULA_SSE", format!("k{}_m{}", k, len)),
                 &k,
                 |b, &k| {
-                    let mut pats = Uniform::from(0..(size - len))
-                        .sample_iter(SmallRng::seed_from_u64(0))
-                        .map(|i| &data[i..i + len]);
                     b.iter(|| {
                         results.clear();
                         ula::search(k as usize, pats.next().unwrap(), &data, &mut results);
@@ -123,10 +101,6 @@ fn bench_data(c: &mut Criterion, url: &str, size: usize) {
                 BenchmarkId::new("triple_accel", format!("k{}_m{}", k, len)),
                 &k,
                 |b, &k| {
-                    let mut pats = Uniform::from(0..(size - len))
-                        .sample_iter(SmallRng::seed_from_u64(0))
-                        .map(|i| &data[i..i + len]);
-
                     b.iter(|| {
                         levenshtein_search_simd_with_opts(
                             pats.next().unwrap(),
@@ -140,6 +114,35 @@ fn bench_data(c: &mut Criterion, url: &str, size: usize) {
                     });
                 },
             );
+
+            // group.bench_with_input(
+            //     BenchmarkId::new("agrep", format!("k{}_m{}", k, len)),
+            //     &k,
+            //     |b, &k| {
+            //         b.iter(|| {
+            //             use std::ffi::OsStr;
+            //             use std::io::Write;
+            //             use std::os::unix::ffi::OsStrExt;
+            //             use std::process::{Command, Stdio};
+            //             let pat = pats.next().unwrap();
+            //             let mut agrep = std::process::Command::new("agrep")
+            //                 .stdin(Stdio::piped())
+            //                 .stdout(Stdio::piped())
+            //                 .arg("--count")
+            //                 .arg(format!("--max-errors={}", k))
+            //                 .arg(OsStr::from_bytes(pat))
+            //                 .spawn()
+            //                 .expect("unable to run agrep");
+            //             {
+            //                 // limited borrow of stdin
+            //                 let stdin = agrep.stdin.as_mut().expect("failed to get stdin");
+            //                 stdin.write_all(data).expect("failed to write to stdin");
+            //             }
+            //             let out = agrep.wait_with_output().expect("failed to wait on child");
+            //             println!("agrep:{:?} pat:{:?}", out, std::str::from_utf8(pat).unwrap());
+            //         });
+            //     },
+            // );
         }
     }
     group.finish();
